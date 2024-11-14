9018117e0989135017f05705ac16b12c2217bc7b
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-14 03:18:25 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 import base64
+import re
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +23,24 @@ class BasicAuth(Auth):
                 return basic_k_v[1]
             return None
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """Basic - Base64 decode"""
+        base64_pattern = re.compile(r'^[A-Za-z0-9+/]+={0,2}$')
+
+        is_str = isinstance(base64_authorization_header, str)
+        if not base64_authorization_header or not is_str:
+            return None
+
+        if not base64_pattern.match(base64_authorization_header):
+            return None
+
+        try:
+            d = base64.b64decode(base64_authorization_header).decode('utf-8')
+            return d
+        except Exception:
+            print("Exception raised")
+            return None
